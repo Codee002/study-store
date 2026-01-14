98f03b60b9857a9d1d9c89372fb1b534aab851de
Code002 <kakashivncm123@gmail.com>
2026-01-15 00:50:19 +0700

    Update Api Route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,14 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ColorController;
+use App\Http\Controllers\Api\DiscountController;
+use App\Http\Controllers\Api\PaymentController;
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\ReceiptController;
+use App\Http\Controllers\Api\SupplierController;
+use App\Http\Controllers\Api\TierController;
+use App\Http\Controllers\Api\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +23,37 @@ Route::prefix("customer")->name("customer.")->group(function () {
         Route::post("/login", [AuthController::class, 'loginCustomer'])->name("login");
         Route::middleware('auth:sanctum')->group(function () {
             Route::get("/me", [AuthController::class, 'me'])->name("me");
+            Route::post("/logout", [AuthController::class, 'logout'])->name("logout");
         });
     });
 });
 
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('suppliers', CategoryController::class);
-Route::apiResource('tiers', CategoryController::class);
+Route::prefix("admin")->name("admin.")->group(function () {
+    Route::prefix("auth")->name("auth.")->group(function () {
+        Route::post("/login", [AuthController::class, 'loginAdmin'])->name("login");
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get("/me", [AuthController::class, 'me'])->name("me");
+            Route::post("/logout", [AuthController::class, 'logout'])->name("logout");
+        });
+    });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('suppliers', SupplierController::class);
+    Route::apiResource('tiers', TierController::class);
+    Route::apiResource('products', ProductController::class);
+    Route::apiResource('payments', PaymentController::class);
+    Route::apiResource('colors', ColorController::class);
+    Route::apiResource('discounts', DiscountController::class);
+
+    // Kho hàng
+    Route::apiResource('warehouses', WarehouseController::class);
+    Route::get('warehouses/{id}/details', [WarehouseController::class, 'details']);
+    Route::patch('warehouse/{warehouseDetailId}/toggle-status', [WarehouseController::class, 'toggleStatus']);
+
+    // Phiếu nhập
+    Route::apiResource('receipts', ReceiptController::class);
+    Route::post('receipts/{id}/approve', [ReceiptController::class, 'approve']);
+    Route::post('receipts/{id}/reject', [ReceiptController::class, 'reject']);
+});
