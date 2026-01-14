96193f38bd2090c3b68401ab1285841a9e916b99
Code002 <kakashivncm123@gmail.com>
2026-01-15 00:15:00 +0700

    Add Migration

---
+++ backend/database/migrations/2026_01_09_121943_modify_column_to_product_images_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            $table->renameColumn("path", 'url');
+            $table->string('public_id')->nullable()->after('url');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_images', function (Blueprint $table) {
+            $table->renameColumn("url", 'path');
+            $table->dropColumn('public_id');
+        });
+    }
+};
---
+++ backend/database/migrations/2026_01_14_104659_add_column_to_warehouse_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Color;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('warehouse_details', function (Blueprint $table) {
+            $table->foreignIdFor(Color::class)->nullable()->after('product_id')->constrained()->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('warehouse_details', function (Blueprint $table) {
+            $table->dropForeign(['color_id']);
+            $table->dropColumn('color_id');
+
+        });
+    }
+};
