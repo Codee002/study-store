b631ef63a01e54eb12e1e2273a6ed8bdc3580e24
Code002 <kakashivncm123@gmail.com>
2026-01-15 00:17:06 +0700

    Update AuthController

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -102,6 +102,60 @@ class AuthController extends Controller
 
     }
 
+    public function loginAdmin(Request $request)
+    {
+        $data = $request->validate([
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::query()
+            ->where('username', $data['username'])
+            ->where('role', 'admin')
+            ->first();
+
+        if (! $user) {
+            return response()->json([
+                "success" => false,
+                'message' => "Tài khoản không tồn tại",
+                'errors'  => [
+                    'username' => ['Tài khoản không tồn tại'],
+                ],
+            ], 422);
+        }
+
+        if ($user->status !== 'actived') {
+            return response()->json([
+                'success' => false,
+                'message' => 'Tài khoản đã bị khóa ',
+                'errors'  => [
+                    'username' => ['Tài khoản đã bị khóa'],
+                ],
+            ], 423);
+        }
+
+        if (! Hash::check($data['password'], $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sai mật khẩu.',
+                'errors'  => [
+                    'password' => ['Sai mật khẩu.'],
+                ],
+            ], 422);
+        }
+
+        $token = $user->createToken('admin_token')->plainTextToken;
+        $user->load("profile");
+        return response()->json([
+            'success'      => true,
+            'message'      => 'Đăng nhập thành công.',
+            'user'         => $user,
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+        ]);
+
+    }
+
     public function me(Request $request)
     {
         $user = $request->user();
@@ -110,6 +164,16 @@ class AuthController extends Controller
             'success' => true,
             'message' => 'Lấy thông tin thành công.',
             'user'    => $user,
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Đăng xuất thành công.',
         ]);
     }
 }
