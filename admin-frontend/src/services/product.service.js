8440bb03a0c408371fb83c3e2d6991a955169b05
Code002 <kakashivncm123@gmail.com>
2026-01-15 00:09:50 +0700

    Update Product View, Service

---
+++ admin-frontend/src/services/product.service.js
@@ -0,0 +1,37 @@
+// src/services/product.service.js
+import { createApiClient } from "./api.service";
+
+class ProductService {
+  constructor(baseUrl = "/api/products") {
+    this.api = createApiClient(baseUrl);
+  }
+
+  async getAll(params = {}) {
+    return (await this.api.get("/", { params })).data;
+  }
+
+  async create(formData) {
+    return (await this.api.post("/", formData)).data;
+  }
+
+  async get(id) {
+    return (await this.api.get(`/${id}`)).data;
+  }
+
+  async update(id, formData) {
+    // console.log(formData);
+    console.log("FormData entries:");
+    for (const pair of formData.entries()) {
+      console.log(pair[0] + ": ", pair[1]);
+    }
+    return (
+      await this.api.post(`/${id}`, formData)
+    ).data;
+  }
+
+  async delete(id) {
+    return (await this.api.delete(`/${id}`)).data;
+  }
+}
+
+export default new ProductService();
